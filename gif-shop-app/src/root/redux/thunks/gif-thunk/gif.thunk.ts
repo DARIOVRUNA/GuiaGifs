fa324b53f22342971e9ea4103c3b36331206779f
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-04-25 10:30:37 -1100

    Implementacion del componente SearchBar.tsx

--- gif-shop-app/src/root/redux/thunks/gif-thunk/gif.thunk.ts
+++ gif-shop-app/src/root/redux/thunks/gif-thunk/gif.thunk.ts
@@ -1,9 +1,11 @@
-import { DispatchType, setGifs } from "../reducers/gif-reducer/gifReducer"; 
-import { gifProvider } from "../provider/gif/gif.provider";
+import { gifProvider } from "../../providers/gif-provider/gif.provider";
+import { DispatchType,setGifs } from "../../reducers/gif-reducer/gifReducer";
+
+
 
 export const startSetGif = (searchTerm: string): any => {
   return async (dispatch: DispatchType) => {
-    const gifList = await gifProvider(searchTerm);
+    const gifList = await gifProvider(searchTerm)
 
     dispatch(setGifs(gifList || []));
   };
